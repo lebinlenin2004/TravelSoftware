1d948b5ae15007a5baf8f5c38c11e95d6c66480e
lebinlenin2004 <lebinlenin2004@gmail.com>
2025-12-27 14:26:13 +0530

    m

--- travel_sales/settings.py
+++ travel_sales/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-change-this-in-production-!@#$%^&*()'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.onrender.com']
 
