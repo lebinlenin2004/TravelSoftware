6e962666c530a87b829a2e60754e9224d8ad85d9
lebinlenin2004 <lebinlenin2004@gmail.com>
2025-12-27 14:22:26 +0530

    Add gunicorn for Render deployment

--- travel_sales/settings.py
+++ travel_sales/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-change-this-in-production-!@#$%^&*()'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
